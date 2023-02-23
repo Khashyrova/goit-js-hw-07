aed238cc5d7e2eddb268138d2bf7e511c988d23e
Khashyrova <121459042+Khashyrova@users.noreply.github.com>
2023-02-23 18:33:47 +0100

    Corrections1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -11,22 +11,6 @@ galleryEl.insertAdjacentHTML('beforeend', itemsGallery);
 
 galleryEl.addEventListener('click', modal);
 
-function createGalery (items) {
-
-    return items.map(({ preview, original, description }) => {
-        return `<div class="gallery__item">
-                    <a class="gallery__link" href= ${original}>
-                        <img
-                            class="gallery__image"
-                            src= ${preview}
-                            data-source= ${original}
-                            alt=${description}
-                        />
-                    </a>
-                </div>`;
-    }).join(''); 
-};
-
 function modal(event) {
     event.preventDefault();
     const datasetSource = event.target.dataset.source;
@@ -44,3 +28,21 @@ function modal(event) {
         instance.close();
     }
 }
+
+function createGalery (items) {
+
+    return items.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+                    <a class="gallery__link" href= ${original}>
+                        <img
+                            class="gallery__image"
+                            src= ${preview}
+                            data-source= ${original}
+                            alt=${description}
+                        />
+                    </a>
+                </div>`;
+    }).join(''); 
+};
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -4,12 +4,9 @@ import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
 
 const galleryEl = document.querySelector('.gallery');
-// console.log(galleryEl);
 const itemsGallery = createGalery(galleryItems);
 galleryEl.insertAdjacentHTML('beforeend', itemsGallery);
-// console.log(itemsGallery);
 
-galleryEl.addEventListener('click', modal);
 
 function createGalery (items) {
 
@@ -26,24 +23,6 @@ function createGalery (items) {
                 </li>`;
     }).join(''); 
 };
-
-function modal(event) {
-    event.preventDefault();
-    const datasetSource = event.target.dataset.source;
-    document.addEventListener('keydown', keyEsc);
-    
-    if (!datasetSource) return;
-    // const instance = basicLightbox.create(`<img src=${datasetSource}>`);  
-
-    instance.show()
-
-    function keyEsc(event) {
-        if (event.code !== 'Escape') return;
-        document.removeEventListener('keydown', keyEsc);
-
-        instance.close();
-    }
-}
 const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
